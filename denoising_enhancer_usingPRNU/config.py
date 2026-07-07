81cd218682000c20f0edfa32fe7ec29ea988d338
Vyapti-Shah <164487749+Vyapti-Shah@users.noreply.github.com>
2026-07-07 18:31:13 +0530

    Delete denoising_enhancer_usingPRNU directory

--- denoising_enhancer_usingPRNU/config.py
+++
@@ -1,8 +0,0 @@
-# config.py
-
-# PRNU Denoising Parameters (DRUNet)
-PRNU_LEVELS = 15
-PRNU_SIGMA = 15.0
-
-# Graphing Options
-PLOT_FILENAME = "denoise_comparison.png"
--- denoising_enhancer_usingPRNU/controller/denoise_controller.py
+++
@@ -1,9 +0,0 @@
-# controller/denoise_controller.py
-from service.pipeline_service import PipelineService
-
-class DenoiseController:
-    def __init__(self):
-        self.pipeline = PipelineService()
-
-    def run(self, input_path, output_path):
-        self.pipeline.process(input_path, output_path)
